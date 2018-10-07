62acf57ca9888f6017a673611ae7edcf8d1bc4e0
Eugene <gish.ee18@gmail.com>
2018-10-07 16:40:05 +0300

    Add base files

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while true; do
+    python3.6 bad-joke-api/main.py
+    exit_code=$?
+
+    if [ $exit_code == 2 ]
+    then
+        echo "Terminate exit code recieved. Exiting"
+        break
+    elif [ $exit_code == 3 ]
+    then
+        seconds=1
+        echo "Restarting without delay"
+    else
+        seconds=15
+    fi
+
+    # git reset --hard
+    git pull origin master
+
+    for ((second=$seconds; second > 0; second--))
+    do
+        echo -ne "Restarting in $second seconds..\r"
+        sleep 1
+    done
+
+    echo
+done
