3c4d8500182a7b22ddf21308a474e047f53fc13d
Eugene <gish.ee18@gmail.com>
2018-10-08 23:18:38 +0300

    Add version endpoint

--- bad-joke-api/main.py
+++ bad-joke-api/main.py
@@ -1,5 +1,9 @@
+import asyncio
+import os
+
 from aiohttp import web
 
+from config import Config
 from updater import updater_task
 
 
@@ -9,9 +13,15 @@ routes = web.RouteTableDef()
 async def index(req):
     return web.Response(text="It works")
 
+@routes.get('/version')
+async def version(req):
+    loop = asyncio.get_event_loop()
+    program = 'git show -s HEAD --format="Currently on commit made %cr by %cn: %s (%H)"'
+    output = await loop.run_in_executor(None, os.popen, program)
+    return web.Response(text=output.read())
+
 async def start_background_tasks(app):
     app['updater'] = app.loop.create_task(updater_task(app))
-
 
 async def cleanup_background_tasks(app):
     app['updater'].cancel()
